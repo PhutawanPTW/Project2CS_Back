902a844bed2958cd25c4e400ccf30cd93f197661
PhutawanPTW <65011212025@msu.ac.th>
2024-03-14 05:07:10 +0700

    new commit

--- api/top.ts
+++ api/top.ts
@@ -5,18 +5,34 @@ import { User, imageUpload } from "../model/model";
 export const router = express.Router();
 
 router.get("/today", (req, res) => {
-  let sql = `
-    SELECT i.userID, i.imageID, u.username, i.url, s.voteScore, s.date,
-      RANK() OVER (ORDER BY s.voteScore DESC) AS rankToday 
-    FROM 
-      images i 
-      JOIN statistics s ON i.imageID = s.imageID
-      JOIN users u ON i.userID = u.userID 
-    WHERE
-      DATE(s.date) = CURRENT_DATE()
-    ORDER BY 
-      s.voteScore DESC 
-    `;
+  // let sql = `
+  //   SELECT i.userID, i.imageID, u.username, i.url, s.voteScore, s.date,
+  //     RANK() OVER (ORDER BY s.voteScore DESC) AS rankToday 
+  //   FROM 
+  //     images i 
+  //     JOIN statistics s ON i.imageID = s.imageID
+  //     JOIN users u ON i.userID = u.userID 
+  //   WHERE
+  //     DATE(s.date) = CURRENT_DATE()
+  //   ORDER BY 
+  //     s.voteScore DESC 
+  //   `;
+
+    let sql = `
+    SELECT 
+    i.userID, 
+    i.imageID, 
+    u.username, 
+    i.url, 
+    i.count,
+
+    RANK() OVER (ORDER BY i.count DESC) AS rankToday 
+FROM 
+    images i 
+JOIN 
+    users u ON i.userID = u.userID 
+ORDER BY 
+    i.count DESC`;
 
   conn.query(sql, (err, result) => {
     if (err) throw err;
@@ -26,18 +42,27 @@ router.get("/today", (req, res) => {
 
 
 router.get("/yesterday", (req, res) => {
+  let date = Date();
+  console.log(date);
   let sql = `
-    SELECT i.userID, i.imageID, u.username, i.url, s.voteScore, s.date,
-      RANK() OVER (ORDER BY s.voteScore DESC) AS rankYesterday 
-    FROM 
-      images i 
-      JOIN statistics s ON i.imageID = s.imageID
-      JOIN users u ON i.userID = u.userID 
-    WHERE 
-      DATE(s.date) = DATE(DATE_SUB(CURDATE(), INTERVAL 1 DAY))
-    ORDER BY 
-      s.voteScore DESC 
-    `;
+    SELECT 
+    i.userID, 
+    i.imageID, 
+    u.username, 
+    i.url, 
+    s.voteScore, 
+    s.date,
+    RANK() OVER (ORDER BY s.voteScore DESC) AS rankYesterday
+FROM 
+    images i 
+JOIN 
+    statistics s ON i.imageID = s.imageID
+JOIN 
+    users u ON i.userID = u.userID 
+WHERE 
+    DATE(s.date) = CURDATE() - INTERVAL 1 DAY
+ORDER BY 
+    s.voteScore DESC`;
 
   conn.query(sql, (err, result) => {
     if (err) throw err;
