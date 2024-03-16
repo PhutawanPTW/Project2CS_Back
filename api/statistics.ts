154889c2eb7f9349736f99854f3df52ad0d0c0a6
PhutawanPTW <65011212025@msu.ac.th>
2024-03-17 02:03:49 +0700

    new commit

--- api/statistics.ts
+++ api/statistics.ts
@@ -3,6 +3,17 @@ import mysql from "mysql";
 import { conn } from "../dbconnect";
 export const router = express.Router();
 
+router.get("/:id", (req, res) => {
+  let id = req.params.id;
+  let sql = 'SELECT * FROM statistics WHERE imageID = ? AND date < CURDATE() - INTERVAL 7  DAY order by date';
+  sql = mysql.format(sql, [id]);
+
+  conn.query(sql, (err, result) => {
+    if (err) throw err;
+    res.status(201).json(result);
+  });
+});
+
 router.get("/:id/:day", (req, res) => {
     let day = req.params.day;
     let id = req.params.id;
